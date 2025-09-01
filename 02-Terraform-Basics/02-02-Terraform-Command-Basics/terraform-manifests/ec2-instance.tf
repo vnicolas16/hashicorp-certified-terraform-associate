094041aede2f4d33103c8dea0f94f850d349c979
vnicolas16 <vnicolas16@yahoo.com>
2025-09-01 17:01:25 -0600

    02-02 update subnet

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -16,6 +16,7 @@ provider "aws" {
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
+  ami           = "ami-00ca32bbc84273381" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
+  subnet_id     = "subnet-0cd1edd336eb850bb"
 }
