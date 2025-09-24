96b73480f942cd2a10b70ce3dc6ab27579c5a935
vnicolas16 <vnicolas16@yahoo.com>
2025-09-23 22:28:56 -0600

    original state

--- 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
@@ -4,11 +4,11 @@
 resource "aws_instance" "my-ec2-vm-Nico" {
   ami               = "ami-08982f1c5bf93d976"
   instance_type     = "t2.micro"
-  #availability_zone = "us-east-1a"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-1a"
+  #availability_zone = "us-east-1b"
   tags = {
     "Name" = "web"
-    "tag1" = "Update-test-1"    
+    #"tag1" = "Update-test-1"    
   }
 }
 
