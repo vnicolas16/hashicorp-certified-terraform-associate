57c92b91439f51347e5bb62340f416dfd1aba1e0
vnicolas16 <vnicolas16@yahoo.com>
2025-09-23 20:52:24 -0600

    use1-az1 (us-east-1a)

--- 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
@@ -4,7 +4,7 @@
 resource "aws_instance" "my-ec2-vm-Nico" {
   ami               = "ami-08982f1c5bf93d976"
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
+  availability_zone = "use1-az1 (us-east-1a)"
   #availability_zone = "us-east-1b"
   tags = {
     "Name" = "web"
