72e8d9a624aec78637945d8d631bd5afd6f85d47
vnicolas16 <vnicolas16@yahoo.com>
2025-09-23 22:07:42 -0600

    new tag

--- 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "my-ec2-vm-Nico" {
   #availability_zone = "us-east-1b"
   tags = {
     "Name" = "web"
-    #"tag1" = "Update-test-1"    
+    "tag1" = "Update-test-1"    
   }
 }
 
