4530bf41f78e54ab3296e148020e93c5737bec59
vnicolas16 <vnicolas16@yahoo.com>
2025-09-23 20:35:43 -0600

    updated AMI

--- 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
@@ -1,8 +1,8 @@
 # Create EC2 Instance
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
-resource "aws_instance" "my-ec2-vm" {
-  ami               = "ami-047a51fa27710816e"
+resource "aws_instance" "my-ec2-vm-Nico" {
+  ami               = "ami-08982f1c5bf93d976"
   instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
   #availability_zone = "us-east-1b"
