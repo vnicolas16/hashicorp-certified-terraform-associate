7ea4f143de93b12749777ef64a56d4c0f127e565
vnicolas16 <vnicolas16@yahoo.com>
2025-10-09 16:31:57 -0600

    count

--- 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-manifests/c2-ec2-instance.tf
@@ -1,10 +1,11 @@
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami           = "ami-047a51fa27710816e" # Amazon Linux
+  ami           = "ami-052064a798f08f0d3" # Amazon Linux
   instance_type = "t2.micro"
+  subnet_id     = "subnet-066bda0f1fd4421c4"
   count         = 5
   tags = {
-    "Name" = "web"
-    #"Name" = "web-${count.index}"
+    #"Name" = "web"
+    "Name" = "web-${count.index}"
   }
 }
