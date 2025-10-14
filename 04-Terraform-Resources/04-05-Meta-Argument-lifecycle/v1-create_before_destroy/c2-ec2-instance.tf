81756eeb16143e028a14d8d22de10978a9c6b396
vnicolas16 <vnicolas16@yahoo.com>
2025-10-14 17:50:35 -0600

    create_before_destroy

--- 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v1-create_before_destroy/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v1-create_before_destroy/c2-ec2-instance.tf
@@ -1,15 +1,18 @@
+
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami               = "ami-0915bcb5fa77e4892" # Amazon Linux
+  ami               = "ami-052064a798f08f0d3" # Amazon Linux
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
+  subnet_id = "subnet-09c76da31475bffe7"
+  #subnet_id =  "subnet-019858be7e0c54cda"
   #availability_zone = "us-east-1b"
+  availability_zone = "us-east-1a"
   tags = {
     "Name" = "web-1"
   }
- /*
+  
   lifecycle {
     create_before_destroy = true
   }
-*/
+
 }
