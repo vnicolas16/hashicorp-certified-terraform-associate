c580944ffe0bc90beefa4763e463ec3cf81eeff4
vnicolas16 <vnicolas16@yahoo.com>
2025-10-15 16:28:19 -0600

    ignore?changes

--- 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v2-prevent_destroy/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v2-prevent_destroy/c2-ec2-instance.tf
@@ -1,12 +1,15 @@
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami = "ami-0915bcb5fa77e4892" # Amazon Linux
+  ami = "ami-052064a798f08f0d3" # Amazon Linux
   instance_type = "t2.micro"
+  subnet_id = "subnet-09c76da31475bffe7"
   tags = {
     "Name" = "web-2"
   }
+/*
   lifecycle {
     prevent_destroy = true # Default is false
   }
+*/
 }
 
--- 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v3-ignore_changes/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v3-ignore_changes/c2-ec2-instance.tf
@@ -1,17 +1,18 @@
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami = "ami-0915bcb5fa77e4892" # Amazon Linux
+  ami = "ami-052064a798f08f0d3" # Amazon Linux
   instance_type = "t2.micro"
+  subnet_id = "subnet-09c76da31475bffe7"
   tags = {
     "Name" = "web-3"
   }
-/*
+
   lifecycle {
     ignore_changes = [
       # Ignore changes to tags, e.g. because a management agent
       # updates these based on some ruleset managed elsewhere.
       tags,
     ]
-  }*/
+  }
 }
 
